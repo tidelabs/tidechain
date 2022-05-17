e8cd6f67fa1f397fe4555d55339af06bd3570b45
Steven Gong <steven.gong@semantic-network.com>
2022-05-17 13:19:50 +0100

    Added more unhappy path tests for submit proposal.

--- frame/quorum/src/tests.rs
+++ frame/quorum/src/tests.rs
@@ -30,6 +30,7 @@ use tidefi_primitives::{
 };
 
 type AccountId = u64;
+type BlockNumber = u64;
 
 const ASSET_1: AssetId = 1u32;
 const ASSET_2: AssetId = 2u32;
@@ -38,18 +39,51 @@ const BOB_ACCOUNT_ID: u32 = 2;
 const ONE_TDFY: u128 = 1_000_000_000_000;
 const BLOCK_NUMBER_ZERO: u64 = 0;
 
+#[derive(Clone)]
+struct UpdateConfiguration {
+  members: Vec<AccountId>,
+  threshold: u16,
+}
+
+#[derive(Clone)]
 struct Context {
   alice: Origin,
   bob: Origin,
   pub_key: Vec<u8>,
   public_keys: BoundedVec<(AccountId, BoundedVec<u8, StringLimit>), PubkeyLimitPerAsset>,
   proposal_id: Hash,
+  valid_mint: Mint<AccountId, Vec<u8>>,
+  valid_withdrawal: Withdrawal<AccountId, BlockNumber, Vec<u8>>,
+  valid_update_configuration: UpdateConfiguration,
+  valid_mint_proposal: ProposalType<AccountId, BlockNumber, Vec<u8>, Vec<AccountId>>,
+  valid_withdrawal_proposal: ProposalType<AccountId, BlockNumber, Vec<u8>, Vec<AccountId>>,
+  valid_update_configuration_proposal:
+    ProposalType<AccountId, BlockNumber, Vec<u8>, Vec<AccountId>>,
 }
 
 impl Default for Context {
   fn default() -> Self {
     let pub_key_bounded_vec: BoundedVec<u8, StringLimit> =
       "pubkey".as_bytes().to_vec().try_into().unwrap();
+    let valid_mint = Mint {
+      account_id: ALICE_ACCOUNT_ID.into(),
+      currency_id: CurrencyId::Tdfy,
+      mint_amount: ONE_TDFY,
+      transaction_id: Vec::new(),
+      compliance_level: ComplianceLevel::Green,
+    };
+    let valid_withdrawal = Withdrawal {
+      account_id: ALICE_ACCOUNT_ID.into(),
+      asset_id: CurrencyId::Tdfy,
+      amount: ONE_TDFY,
+      external_address: Vec::new(),
+      block_number: BLOCK_NUMBER_ZERO,
+    };
+    let valid_update_configuration = UpdateConfiguration {
+      members: vec![ALICE_ACCOUNT_ID.into(), BOB_ACCOUNT_ID.into()],
+      threshold: 1u16,
+    };
+
     Self {
       alice: Origin::signed(ALICE_ACCOUNT_ID.into()),
       bob: Origin::signed(BOB_ACCOUNT_ID.into()),
@@ -61,6 +95,15 @@ impl Default for Context {
         "0x02a204a25c36f8c88eea76e08cdaa22a0569ef630bf4416db72abb9fb2445f2b",
       )
       .unwrap(),
+      valid_mint: valid_mint.clone(),
+      valid_withdrawal: valid_withdrawal.clone(),
+      valid_update_configuration: valid_update_configuration.clone(),
+      valid_mint_proposal: ProposalType::Mint(valid_mint.clone()),
+      valid_withdrawal_proposal: ProposalType::Withdrawal(valid_withdrawal.clone()),
+      valid_update_configuration_proposal: ProposalType::UpdateConfiguration(
+        valid_update_configuration.members,
+        valid_update_configuration.threshold,
+      ),
     }
   }
 }
@@ -71,6 +114,19 @@ impl Context {
     assert!(Members::<Test>::contains_key(ALICE_ACCOUNT_ID as u64));
     assert_eq!(PublicKeys::<Test>::get(ASSET_1).len(), 1);
     self
+  }
+
+  fn get_valid_proposals(
+    &self,
+  ) -> Vec<ProposalType<AccountId, BlockNumber, Vec<u8>, Vec<AccountId>>> {
+    vec![
+      ProposalType::Mint(self.valid_mint.clone()),
+      ProposalType::Withdrawal(self.valid_withdrawal.clone()),
+      ProposalType::UpdateConfiguration(
+        self.valid_update_configuration.members.clone(),
+        self.valid_update_configuration.threshold,
+      ),
+    ]
   }
 }
 
@@ -92,17 +148,9 @@ mod submit_proposal {
       new_test_ext().execute_with(|| {
         let context = Context::default().insert_asset1_with_alice_public_key();
 
-        let mint_proposal = ProposalType::Mint(Mint {
-          account_id: ALICE_ACCOUNT_ID.into(),
-          currency_id: CurrencyId::Tdfy,
-          mint_amount: ONE_TDFY,
-          transaction_id: Vec::new(),
-          compliance_level: ComplianceLevel::Green,
-        });
-
         assert_ok!(Quorum::submit_proposal(
           context.alice.clone(),
-          mint_proposal
+          context.valid_mint_proposal
         ));
 
         assert_eq!(
@@ -111,11 +159,11 @@ mod submit_proposal {
             context.proposal_id,
             BLOCK_NUMBER_ZERO,
             ProposalType::Mint(Mint {
-              account_id: ALICE_ACCOUNT_ID.into(),
-              currency_id: CurrencyId::Tdfy,
-              mint_amount: ONE_TDFY,
-              transaction_id: BoundedVec::try_from(Vec::new()).unwrap(),
-              compliance_level: ComplianceLevel::Green,
+              account_id: context.valid_mint.account_id,
+              currency_id: context.valid_mint.currency_id,
+              mint_amount: context.valid_mint.mint_amount,
+              transaction_id: BoundedVec::try_from(context.valid_mint.transaction_id).unwrap(),
+              compliance_level: context.valid_mint.compliance_level,
             })
           )
         );
@@ -127,17 +175,9 @@ mod submit_proposal {
       new_test_ext().execute_with(|| {
         let context = Context::default().insert_asset1_with_alice_public_key();
 
-        let withdrawal_proposal = ProposalType::Withdrawal(Withdrawal {
-          account_id: ALICE_ACCOUNT_ID.into(),
-          asset_id: CurrencyId::Tdfy,
-          amount: ONE_TDFY,
-          external_address: Vec::new(),
-          block_number: BLOCK_NUMBER_ZERO,
-        });
-
         assert_ok!(Quorum::submit_proposal(
           context.alice.clone(),
-          withdrawal_proposal
+          context.valid_withdrawal_proposal
         ));
 
         assert_eq!(
@@ -146,11 +186,12 @@ mod submit_proposal {
             context.proposal_id,
             BLOCK_NUMBER_ZERO,
             ProposalType::Withdrawal(Withdrawal {
-              account_id: ALICE_ACCOUNT_ID.into(),
-              asset_id: CurrencyId::Tdfy,
-              amount: ONE_TDFY,
-              external_address: BoundedVec::try_from(Vec::new()).unwrap(),
-              block_number: BLOCK_NUMBER_ZERO,
+              account_id: context.valid_withdrawal.account_id,
+              asset_id: context.valid_withdrawal.asset_id,
+              amount: context.valid_withdrawal.amount,
+              external_address: BoundedVec::try_from(context.valid_withdrawal.external_address)
+                .unwrap(),
+              block_number: context.valid_withdrawal.block_number,
             })
           )
         );
@@ -162,12 +203,9 @@ mod submit_proposal {
       new_test_ext().execute_with(|| {
         let context = Context::default().insert_asset1_with_alice_public_key();
 
-        let new_members = vec![ALICE_ACCOUNT_ID.into(), BOB_ACCOUNT_ID.into()];
-        let new_threshold = 1u16;
-
         assert_ok!(Quorum::submit_proposal(
           context.alice,
-          ProposalType::UpdateConfiguration(new_members.clone(), new_threshold)
+          context.valid_update_configuration_proposal
         ));
 
         assert_eq!(
@@ -176,11 +214,40 @@ mod submit_proposal {
             context.proposal_id,
             BLOCK_NUMBER_ZERO,
             ProposalType::UpdateConfiguration(
-              BoundedVec::try_from(new_members).unwrap(),
-              new_threshold
+              BoundedVec::try_from(context.valid_update_configuration.members).unwrap(),
+              context.valid_update_configuration.threshold
             )
           )
         );
+      });
+    }
+  }
+
+  mod fails_when {
+    use super::*;
+
+    #[test]
+    pub fn sender_is_not_signed() {
+      new_test_ext().execute_with(|| {
+        let context = Context::default().insert_asset1_with_alice_public_key();
+
+        for proposal in context.get_valid_proposals() {
+          assert_noop!(Quorum::submit_proposal(Origin::none(), proposal), BadOrigin);
+        }
+      });
+    }
+
+    #[test]
+    pub fn sender_is_not_a_member() {
+      new_test_ext().execute_with(|| {
+        let context = Context::default().insert_asset1_with_alice_public_key();
+
+        for proposal in context.get_valid_proposals() {
+          assert_noop!(
+            Quorum::submit_proposal(context.bob.clone(), proposal),
+            Error::<Test>::AccessDenied
+          );
+        }
       });
     }
   }
