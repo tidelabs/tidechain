f534cbcfdc87ebe6b2bb5947842d9170a55234ee
david <david@lemarier.ca>
2022-09-12 07:32:54 -0400

    add `pallet_security` weight
    Signed-off-by: david <david@lemarier.ca>

--- runtime/common/config/tidefi.rs
+++ runtime/common/config/tidefi.rs
@@ -199,7 +199,7 @@ impl pallet_oracle::Config for Runtime {
 
 impl pallet_security::Config for Runtime {
   type Event = Event;
-  type WeightInfo = pallet_security::weights::SubstrateWeight<Runtime>;
+  type WeightInfo = crate::weights::pallet_security::WeightInfo<Runtime>;
 }
 
 impl pallet_asset_registry::Config for Runtime {
