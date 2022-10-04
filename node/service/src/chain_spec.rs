766ea46a1bf79974c37685053c80456f3205232e
Steven Gong <103124034+steven-semnet@users.noreply.github.com>
2022-10-04 12:18:17 +0100

    Assert trading rebates total (#213)
    * Add vesting pallet
    
    Signed-off-by: david <david@lemarier.ca>
    
    * fix benchmarks
    
    Signed-off-by: david <david@lemarier.ca>
    
    * Added threes years period
    
    * Added initial account allocation and vesting schedules for accounts.
    
    * Adjust stakeholders and vesting accounts
    
    Signed-off-by: david <david@lemarier.ca>
    
    * cargo run --quiet --profile=production  --features=runtime-benchmarks -- benchmark pallet --chain=tidechain-dev --steps=50 --repeat=20 --pallet=* --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --header=./FILE_TEMPLATE --output=./runtime/tidechain/src/weights/
    
    * Assert vesting periods totals.
    
    * Assert tiered rebates total from sunrise pool.
    
    * Added a new account and assert initial account allocations.
    
    * Renamed constants
    
    * Renamed constants and variables.
    
    * Removed the 15mins staking period
    
    * split chain spec in 2 process
    
    Signed-off-by: david <david@lemarier.ca>
    
    * Added some accounts to chain spec.
    
    * Code tidy up.
    
    Signed-off-by: david <david@lemarier.ca>
    Co-authored-by: david <david@lemarier.ca>
    Co-authored-by: Tidechain CI <tidechain.ci@gmail.com>

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -137,7 +137,9 @@ fn lagoon_testnet_genesis(
   const ENDOWMENT: u128 = 10_500 * 1_000_000_000_000;
   const TOTAL_SUPPLY: u128 = 1_000_000_000 * 1_000_000_000_000;
   const STASH: u128 = 10_000 * 1_000_000_000_000;
-  const SUNRISE_POOL: u128 = (192_000_000 + 48_000_000) * 1_000_000_000_000;
+  const ON_BOARDING_REBATES: u128 = 48_000_000 * 1_000_000_000_000;
+  const TRADING_REBATES: u128 = 192_000_000 * 1_000_000_000_000;
+  const SUNRISE_POOL: u128 = ON_BOARDING_REBATES + TRADING_REBATES;
   // Treasury Account Id
   let treasury_account: AccountId =
     lagoon_runtime::TreasuryPalletId::get().into_account_truncating();
@@ -204,6 +206,17 @@ fn lagoon_testnet_genesis(
   );
 
   let vesting = helpers::get_vesting_terms_lagoon();
+
+  let sunrise = crate::tidefi_sunrise_pool_genesis!(lagoon_runtime);
+  let trading_rebates_total: Balance = sunrise
+    .swap_pools
+    .iter()
+    .map(|swap_pool| swap_pool.balance)
+    .sum();
+  assert_eq!(
+    trading_rebates_total, TRADING_REBATES,
+    "Sunrise pool trading rebates total is not correct"
+  );
 
   lagoon_runtime::GenesisConfig {
     system: lagoon_runtime::SystemConfig {
@@ -311,7 +324,7 @@ fn lagoon_testnet_genesis(
     security: Default::default(),
     fees: Default::default(),
     vesting: lagoon_runtime::VestingConfig { vesting },
-    sunrise: crate::tidefi_sunrise_pool_genesis!(lagoon_runtime),
+    sunrise: sunrise,
     tidefi_staking: crate::tidefi_staking_genesis!(lagoon_runtime),
   }
 }
@@ -337,7 +350,9 @@ fn tidechain_testnet_genesis(
   const ENDOWMENT: u128 = 10_500 * 1_000_000_000_000;
   const TOTAL_SUPPLY: u128 = 1_000_000_000 * 1_000_000_000_000;
   const STASH: u128 = 10_000 * 1_000_000_000_000;
-  const SUNRISE_POOL: u128 = (192_000_000 + 48_000_000) * 1_000_000_000_000;
+  const ON_BOARDING_REBATES: u128 = 48_000_000 * 1_000_000_000_000;
+  const TRADING_REBATES: u128 = 192_000_000 * 1_000_000_000_000;
+  const SUNRISE_POOL: u128 = ON_BOARDING_REBATES + TRADING_REBATES;
 
   const VESTING_TOTAL_FOR_ONE_MONTH_PERIOD: u128 = 2_382_910 * 1_000_000_000_000;
   const VESTING_TOTAL_FOR_SIX_MONTHS_PERIOD: u128 = 2_382_910 * 1_000_000_000_000;
@@ -449,6 +464,17 @@ fn tidechain_testnet_genesis(
     "Total vesting at the end of the three years is not correct"
   );
 
+  let sunrise = crate::tidefi_sunrise_pool_genesis!(tidechain_runtime);
+  let trading_rebates_total: Balance = sunrise
+    .swap_pools
+    .iter()
+    .map(|swap_pool| swap_pool.balance)
+    .sum();
+  assert_eq!(
+    trading_rebates_total, TRADING_REBATES,
+    "Sunrise pool trading rebates total is not correct"
+  );
+
   tidechain_runtime::GenesisConfig {
     system: tidechain_runtime::SystemConfig {
       code: wasm_binary.to_vec(),
@@ -535,7 +561,7 @@ fn tidechain_testnet_genesis(
     security: Default::default(),
     fees: Default::default(),
     vesting: tidechain_runtime::VestingConfig { vesting },
-    sunrise: crate::tidefi_sunrise_pool_genesis!(tidechain_runtime),
+    sunrise: sunrise,
     tidefi_staking: crate::tidefi_staking_genesis!(tidechain_runtime),
   }
 }
@@ -1050,8 +1076,6 @@ mod helpers {
           initial_amount: assets::Asset::Tdfy.saturating_mul(48_000_000),
           available_amount: assets::Asset::Tdfy.saturating_mul(48_000_000),
         }),
-        // FIXME: Maybe add some validation to make sure it equals `192_000_000`
-        // 67200000 + 57600000 + 38400000 + 19200000 + 9600000 = 192_000_000
         swap_pools: vec![
           SunriseSwapPool {
             id: 1,
