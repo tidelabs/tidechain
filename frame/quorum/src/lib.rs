c0f30112cf6de754c767fe8c5eed18f3e9596161
david <david@lemarier.ca>
2023-03-02 08:57:07 -0500

    Use stable rust for clippy
    Related to https://github.com/clap-rs/clap/issues/4733
    
    Signed-off-by: david <david@lemarier.ca>

--- frame/quorum/src/lib.rs
+++ frame/quorum/src/lib.rs
@@ -13,7 +13,6 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Tidechain.  If not, see <http://www.gnu.org/licenses/>.
-#![allow(clippy::type_complexity)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(test)]
--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Tidechain.  If not, see <http://www.gnu.org/licenses/>.
 
-#![allow(clippy::type_complexity)]
-
 #[cfg(feature = "full-node")]
 pub use tidechain_client::{
   AbstractClient, Client, ClientHandle, ExecuteWithClient, FullBackend, FullClient,
