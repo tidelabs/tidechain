99546f2e010d0174037fed4da433b2ed04a33c0b
Steven Gong <103124034+steven-semnet@users.noreply.github.com>
2023-06-12 16:18:37 +0100

    Removed previously completed migrations and fixed github workflow (#326)
    * Updated github action versions. (#323)
    
    * Updated release candidate workflow script (#324)
    
    * Removed previously completed migrations
    
    * Renamed CurrenciesMetadata to CurrencyMetadata
    
    * Use custom weight

--- frame/tidefi-rpc/runtime-api/src/lib.rs
+++ frame/tidefi-rpc/runtime-api/src/lib.rs
@@ -22,7 +22,7 @@ use tidefi_primitives::{
   BalanceInfo, BlockNumber, CurrencyBalance, CurrencyId, CurrencyMetadata, Stake,
 };
 
-type CurrenciesMetadata = (CurrencyId, CurrencyMetadata<Vec<u8>>);
+type CurrencyMetadata = (CurrencyId, CurrencyMetadata<Vec<u8>>);
 type CurrenciesStake = (CurrencyId, Stake<BalanceInfo, BlockNumber>);
 
 sp_api::decl_runtime_apis! {
@@ -31,6 +31,6 @@ sp_api::decl_runtime_apis! {
             fn get_account_balance(account_id: AccountId, asset_id: CurrencyId) -> Result<CurrencyBalance<BalanceInfo>, DispatchError>;
             fn get_account_balances(account_id: AccountId) -> Result<Vec<(CurrencyId, CurrencyBalance<BalanceInfo>)>, DispatchError>;
             fn get_account_stakes(account_id: AccountId) -> Result<Vec<CurrenciesStake>, DispatchError>;
-            fn get_assets() -> Result<Vec<CurrenciesMetadata>, DispatchError>;
+            fn get_assets() -> Result<Vec<CurrencyMetadata>, DispatchError>;
         }
 }
